26a301ad57e827b856e0c4d9a5a9df73ae6af231
LucasAlfare <god-lucas@hotmail.com>
2024-02-11 02:54:35 -0300

    atualizando documentações de estudo

--- app/src/main/kotlin/com/lucasalfare/rinhadebackend2024/App.kt
+++ app/src/main/kotlin/com/lucasalfare/rinhadebackend2024/App.kt
@@ -11,6 +11,11 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
+/**
+ * Função principal responsável por iniciar o servidor embutido utilizando Netty.
+ * Configura o Content Negotiation para fornecer respostas em formato JSON.
+ * Define rotas para manipulação de transações e obtenção de extrato bancário.
+ */
 fun main() {
   embeddedServer(Netty, 9999) {
     install(ContentNegotiation) {
@@ -21,9 +26,14 @@ fun main() {
     }
 
     routing {
-      /*
-      curl -v -d '{"valor": 1000, "tipo" : "d", "descricao" : "pgto?"}' -H 'Content-Type: application/json' http://localhost:9999/clientes/1/transacoes
-      */
+      /**
+       * Rota para criação de uma nova transação para um cliente específico.
+       * Utiliza o método HTTP POST para enviar os dados da transação no corpo da requisição.
+       * Exemplo de uso:
+       * ```
+       * curl -v -d '{"valor": 1000, "tipo" : "d", "descricao" : "pgto?"}' -H 'Content-Type: application/json' http://localhost:9999/clientes/1/transacoes
+       * ```
+       */
       post("/clientes/{id}/transacoes") {
         val id = call.parameters["id"]!!.toInt()
 
@@ -35,22 +45,27 @@ fun main() {
         // retornar o erro correspondente (que será proveniente
         // do bloco "init" de [TransactionRequestDTO].
         // Além disso, podemos obter erros relacionados à má
-        // deserrialização.
+        // deserialização.
         runCatching {
           val dto = call.receive<TransactionRequestDTO>()
-          val myResponse = Database.createTransaction(id, dto)
+          val myResponse = InMemoryDatabase.createTransaction(id, dto)
           call.respond(myResponse.code, myResponse.data)
         }.onFailure {
           call.respond(HttpStatusCode.BadRequest, it.cause?.message ?: "")
         }
       }
 
-      /*
-      curl http://localhost:9999/clientes/1/extrato
+      /**
+       * Rota para obtenção do extrato bancário de um cliente específico.
+       * Utiliza o método HTTP GET para realizar a requisição.
+       * Exemplo de uso:
+       * ```
+       * curl http://localhost:9999/clientes/1/extrato
+       * ```
        */
       get("/clientes/{id}/extrato") {
         val id = call.parameters["id"]!!.toInt()
-        val myResponse = Database.getBankStatement(id)
+        val myResponse = InMemoryDatabase.getBankStatement(id)
 
         call.respond(myResponse.code, myResponse.data)
       }
--- app/src/main/kotlin/com/lucasalfare/rinhadebackend2024/InMemoryDatabase.kt
+++ app/src/main/kotlin/com/lucasalfare/rinhadebackend2024/InMemoryDatabase.kt
@@ -2,8 +2,12 @@ package com.lucasalfare.rinhadebackend2024
 
 import io.ktor.http.*
 
-object Database {
+/**
+ * Objeto responsável por gerenciar o acesso aos dados do banco de dados simulado.
+ */
+object InMemoryDatabase {
 
+  // Lista de clientes simulados com seus limites e saldos iniciais.
   private val clients = mutableListOf(
     Client(id = 1, limit = 100000, balance = 0),
     Client(id = 2, limit = 80000, balance = 0),
@@ -12,6 +16,12 @@ object Database {
     Client(id = 5, limit = 500000, balance = 0)
   )
 
+  /**
+   * Cria uma nova transação para um cliente específico com base nos dados fornecidos.
+   * @param id O ID do cliente.
+   * @param transactionRequestDTO Os dados da transação a ser criada.
+   * @return O resultado da operação, indicando sucesso ou falha.
+   */
   fun createTransaction(
     id: Int,
     transactionRequestDTO: TransactionRequestDTO
@@ -55,6 +65,11 @@ object Database {
     return OperationResult(HttpStatusCode.NotFound)
   }
 
+  /**
+   * Obtém o extrato bancário de um cliente específico.
+   * @param id O ID do cliente.
+   * @return O resultado da operação, contendo o extrato bancário ou indicando falha.
+   */
   fun getBankStatement(id: Int): OperationResult {
     clients.find { it.id == id }?.let {
       val bankStatement = BankStatement(
